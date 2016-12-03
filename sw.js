3276e506014352ff36a947829724bf0447f364c0
Nikhil Nayak <nikhilnayak98@gmail.com>
2016-12-03 23:41:05 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,64 +1,31 @@
-self.CACHE_NAME = 'nikhilnayak-v1';
-if(typeof self.CACHE_NAME !== 'string') {
-	throw new Error('Cache Name cannot be empty');
-}
-
-self.addEventListener('fetch', function(event) {
-
-	// Clone the request for fetch and cache
-	// A request is a stream and can be consumed only once.
-	var fetchRequest = event.request.clone(),
-		cacheRequest = event.request.clone();
-
-	// Respond with content from fetch or cache
-	event.respondWith(
-
-		// Try fetch
-		fetch(fetchRequest)
-
-			// when fetch is successful, we update the cache
-			.then(function(response) {
-
-				// A response is a stream and can be consumed only once.
-				// Because we want the browser to consume the response,
-				// as well as cache to consume the response, we need to
-				// clone it so we have 2 streams
-				var responseToCache = response.clone();
-
-				// and update the cache
-				caches
-					.open(self.CACHE_NAME)
-					.then(function(cache) {
-
-						// Clone the request again to use it
-						// as the key for our cache
-						var cacheSaveRequest = event.request.clone();
-						cache.put(cacheSaveRequest, responseToCache);
-
-					});
-
-				// Return the response stream to be consumed by browser
-				return response;
-
-			})
-
-			// when fetch times out or fails
-			.catch(function(err) {
-
-				// Return the promise which
-				// resolves on a match in cache for the current request
-				// ot rejects if no matches are found
-				return caches.match(cacheRequest);
-
-			})
-	);
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('nikhilnayak').then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/css/main.css',
+	'https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/css/materialize.min.css',
+	'https://fonts.googleapis.com/icon?family=Material+Icons',
+	'https://fonts.googleapis.com/css?family=Roboto:400,400italic,700|Roboto+Mono:400,500',
+	'https://code.jquery.com/jquery-2.1.1.min.js',
+        'https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/js/materialize.min.js',
+        'https://nikhilnayak98.github.io/js/smoothscroll.js',
+	'https://nikhilnayak98.github.io/js/smoothscroll.js'
+      ])
+      .then(() => self.skipWaiting());
+    })
+  )
 });
 
-// Now we need to clean up resources in the previous versions
-// of Service Worker scripts
-self.addEventListener('activate', function(event) {
+self.addEventListener('activate',  event => {
+  event.waitUntil(self.clients.claim());
+});
 
-	// Destroy the cache
-	event.waitUntil(caches.delete(self.CACHE_NAME));
-
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
