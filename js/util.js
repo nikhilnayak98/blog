a0c2b05a9a7d987e7044a8941e30f4777eea83d2
Nikhil Nayak <nikhilnayak98@gmail.com>
2016-12-14 18:49:46 +0530

    Update util.js

--- js/util.js
+++ js/util.js
@@ -1,12 +1,12 @@
       function updateOfflineStatus(){
-        var $toastOff = $('<span style="font-size:18px;font-style: normal;">You are offline.</span>');
+        var $toastOff = $('<span style="font-size:18px;font-style: normal;">You are offline<img src="/images/icons/svgs/offline.svg"/></span>');
         Materialize.toast($toastOff, 5000);
         mess=1;
       }
       var mess=0;
       function updateOnlineStatus(){
         if(mess==1){
-         var $toastOn = $('<span style="font-size:18px;font-style: normal;">You are back online.</span>');
+         var $toastOn = $('<span style="font-size:18px;font-style: normal;">You are back online<img src="/images/icons/svgs/check.svg" /></span>');
          Materialize.toast($toastOn, 5000);
         }
       }
