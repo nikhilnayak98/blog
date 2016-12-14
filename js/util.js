83c7029f8441b141e3298b1ada56534171d17b7d
Nikhil Nayak <nikhilnayak98@gmail.com>
2016-12-14 18:39:44 +0530

    Update util.js

--- js/util.js
+++ js/util.js
@@ -1,11 +1,3 @@
-function alertShow(){
-       var alerted = localStorage.getItem('alerted') || '';
-        if (alerted != 'yes') {
-         var $toastContent = $('<span style="font-size:18px;font-style: normal;">Caching Complete<img src="/images/icons/svgs/check.svg"/></span>');
-         Materialize.toast($toastContent, 4000, '', function(){var $toastContents = $('<span style="font-size:18px;font-style: normal;">Future Visits Will Work Offline.</span>');Materialize.toast($toastContents, 4000);});
-         localStorage.setItem('alerted','yes');
-        }
-      }
       function updateOfflineStatus(){
         var $toastOff = $('<span style="font-size:18px;font-style: normal;">You are offline.</span>');
         Materialize.toast($toastOff, 5000);
