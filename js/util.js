453d897b816f9a80fcb7c9c219871f96339e8e12
Nikhil Nayak <nikhilnayak98@gmail.com>
2016-12-20 19:39:55 +0530

    Update util.js

--- js/util.js
+++ js/util.js
@@ -1,14 +1,26 @@
-      function updateOfflineStatus(){
-        var $toastOff = $('<span style="font-size:18px;font-style: normal;">You are offline <img src="/images/icons/svgs/offline.svg"/></span>');
-        Materialize.toast($toastOff, 5000);
-        mess=1;
-      }
-      var mess=0;
-      function updateOnlineStatus(){
-        if(mess==1){
-         var $toastOn = $('<span style="font-size:18px;font-style: normal;">You are back online <img src="/images/icons/svgs/check.svg" /></span>');
-         Materialize.toast($toastOn, 5000);
-        }
-      }
-     window.addEventListener('offline', updateOfflineStatus);
-     window.addEventListener('online', updateOnlineStatus);
+var mess=0;      
+function updateOfflineStatus(){
+  var $toastOff = $('<span style="font-size:18px;font-style: normal;">You are offline <img src="/images/icons/svgs/offline.svg"/></span>');
+  Materialize.toast($toastOff, 5000);
+  changeColorOffline();
+  mess=1;
+}
+function updateOnlineStatus(){
+   if(mess==1){
+    var $toastOn = $('<span style="font-size:18px;font-style: normal;">You are back online <img src="/images/icons/svgs/check.svg" /></span>');
+    Materialize.toast($toastOn, 5000);
+   }
+  changeColorOnline();
+}
+function changeColorOffline(){
+   document.getElementsByTagName("HTML")[0].setAttribute("class", "t-grey");
+   document.getElementsByClassName("btn-floating btn-large purple darken-4")[0].setAttribute("class", "btn-floating btn-large grey darken-1");
+   document.getElementsByClassName("tabs")[0].style.background='grey';
+}
+function changeColorOnline(){
+   document.getElementsByTagName("HTML")[0].setAttribute("class", "t-purple");
+   document.getElementsByClassName("btn-floating btn-large grey darken-1")[0].setAttribute("class", "btn-floating btn-large purple darken-4");
+   document.getElementsByClassName("tabs")[0].style.background='#673AB7';
+}
+window.addEventListener('offline', updateOfflineStatus);
+window.addEventListener('online', updateOnlineStatus);
